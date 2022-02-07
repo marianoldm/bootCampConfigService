9d09ed22703da8047a6a9b370fa0c87801eca4b3
marianoldm <m.lorendelamo@gmail.com>
2022-02-07 18:25:17 +0100

    eureka

--- customer-service-new/src/main/java/academy/digitallab/store/customer/repository/entity/Region.java
+++ customer-service-new/src/main/java/academy/digitallab/store/customer/repository/entity/Region.java
@@ -1,6 +1,7 @@
 package academy.digitallab.store.customer.repository.entity;
 
 import lombok.Data;
+import org.hibernate.annotations.Proxy;
 
 import javax.persistence.*;
 import java.io.Serializable;
@@ -8,6 +9,7 @@ import java.io.Serializable;
 @Data
 @Entity
 @Table(name = "tbl_regions")
+@Proxy(lazy = false)
 public class Region implements Serializable {
 
 	@Id
