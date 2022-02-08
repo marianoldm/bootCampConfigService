4fc4f63b6363b1ed00d65ddbfd1de9a899bebe33
marianoldm <m.lorendelamo@gmail.com>
2022-02-08 23:26:10 +0100

    fb

--- customer-service-new/src/main/resources/data.sql
+++ customer-service-new/src/main/resources/data.sql
@@ -1,3 +1,10 @@
+DROP TABLE IF EXISTS tbl_regions;
+
+CREATE TABLE tbl_regions (
+  id INT PRIMARY KEY,
+  name VARCHAR(50) NOT NULL
+);
+
 INSERT INTO tbl_regions (id, name) VALUES (1, 'Sudamérica');
 INSERT INTO tbl_regions (id, name) VALUES (2, 'Centroamérica');
 INSERT INTO tbl_regions (id, name) VALUES (3, 'Norteamérica');
@@ -6,5 +13,18 @@ INSERT INTO tbl_regions (id, name) VALUES (5, 'Asia');
 INSERT INTO tbl_regions (id, name) VALUES (6, 'Africa');
 INSERT INTO tbl_regions (id, name) VALUES (7, 'Oceanía');
 INSERT INTO tbl_regions (id, name) VALUES (8, 'Antártida');
+
+DROP TABLE IF EXISTS tbl_customers;
+
+CREATE TABLE tbl_customers (
+  id INT PRIMARY KEY,
+  number_id VARCHAR(50) NOT NULL ,
+  first_name VARCHAR(50) NOT NULL,
+  last_name VARCHAR(50) NOT NULL,
+  email VARCHAR(50) NOT NULL,
+  photo_url VARCHAR(50),
+  region_id INT NOT NULL,
+  state VARCHAR(50) NOT NULL
+);
 
 INSERT INTO tbl_customers (id,number_id,first_name,last_name , email, photo_url, region_id, state) VALUES(1,'32404580', 'Andrés', 'Guzmán', 'profesor@bolsadeideas.com','',1,'CREATED');
